b57875e32c365878b36a5fbe63f72f2aef140c47
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2026-06-05 20:00:39 -0400

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # anagramMap = {}
+        # output = []
+
+        # for i in strs:
+        #     unscrambled = "".join(sorted(i))
+        #     # print(unscrambled)
+        #     anagramMap.setdefault(unscrambled, []).append(i)
+        
+        # for i in anagramMap:
+        #     output.append(anagramMap[i])
+        
+        # return output
+
+        anagrams = {}
+
+        for s in strs:
+            unscrambled = [0] * 26
+            for i in s:
+                unscrambled[ord('a') - ord(i)] += 1
+            key = tuple(unscrambled)
+            anagrams.setdefault(key, []).append(s)
+        
+        return list(anagrams.values())
