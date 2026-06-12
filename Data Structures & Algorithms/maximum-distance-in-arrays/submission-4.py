285f41b48d81c4476ec439a2131d7abb739b4ab4
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2026-06-12 17:48:28 -0400

    Add: maximum-distance-in-arrays - submission-4

---
+++ Data Structures & Algorithms/maximum-distance-in-arrays/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxDistance(self, arrays: List[List[int]]) -> int:
+        currentMin, currentMax = arrays[0][0], arrays[0][-1]
+
+        dist = 0
+
+        for i in range(1, len(arrays)):
+            dist = max(abs(arrays[i][-1] - currentMin), abs(currentMax - arrays[i][0]), dist)
+
+            currentMin = min(arrays[i][0], currentMin)
+            currentMax = max(arrays[i][-1], currentMax)
+        
+        return dist
+
