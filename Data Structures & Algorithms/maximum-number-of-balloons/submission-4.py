54f58f53571be53a54a2616eb8d46091c4585261
Manov Jain <47869311+ManovJain@users.noreply.github.com>
2026-06-14 11:26:04 -0400

    Add: maximum-number-of-balloons - submission-4

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        bMap = defaultdict(int)
+
+        for i in text:
+            if i in "balloon":
+                bMap[i] += 1
+
+        bMap['l'] = bMap['l'] // 2
+        bMap['o'] = bMap['o'] // 2
+
+        return min(bMap.values())
